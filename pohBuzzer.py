afed51624e3ee7aa27a10f78552129a3f1add2b7
Andres <andres1900escribanos@gmail.com>
2019-09-20 14:45:32 -0500

    prueba de buzzer

--- pohBuzzer.py
+++ pohBuzzer.py
@@ -1,12 +1,28 @@
 import gpiozero
+from smbus2 import SMBus
+import time
+address = 0x20
 
-from gpiozero import Buzzer
-from time import sleep
+class zum():
+   def __int__(self,busNumber=1, address=0x20):
+      self.bus = SMBus(busNumber)
+      self.address = address
 
-buzzer = Buzzer(7)
-while True:
-   buzzer.on()
-   sleep(1)
-   buzzer.off()
-   sleep(1)
+   def beep_on():
+    	bus.write_byte(address,0x7F&bus.read_byte(address)) #read one byte from address, offset 0
+   def beep_off():
+      bus.write_byte(address,0x80|bus.read_byte(address)) #read one byte from address, offset 0
+
+   def Encender(self,sleepTime=1):
+      beep_on()
+      print("Encendido")    
+      sleep(sleepTime)
+
+   def Apagar(self,sleepTime=1):
+      beep_off()
+      print("Apagado")
+      sleep(sleepTime)
+      
+
+
 
