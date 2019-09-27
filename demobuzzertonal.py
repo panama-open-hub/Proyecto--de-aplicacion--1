f655699f90c23b40b4564a1a701d2d2519a4a183
Javier Batista <javieribatista29@gmail.com>
2019-09-27 16:09:00 -0500

    Agregado TwinkleRemixBuzzerTonal

--- demobuzzertonal.py
+++ demobuzzertonal.py
@@ -2,18 +2,18 @@ from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
 from time import sleep
 t = TonalBuzzer(27) # change to whatever pin the buzzer is connected
-t.play(Tone("A2"))
-sleep(1)
-t.stop()
-sleep(0.1)
 
 
-"""
-v1 = ["G4", "G4", "G4", "D4", "E4", "E4", "D4"]
-v2 = ["B4", "B4", "A4", "A4", "G4"]
-v3 = ["D4", "G4", "G4", "G4", "D4", "E4", "E4", "D4"]
+v1 = ["C4", "C4", "G4","G4","A4","A4","G4"]
+v2 = ["F4","F4","E4","E4","D4","D4","C4"]
+v3 = ["G4", "G4", "F4","F4","E4","E4","D4"]
+v4 = ["C4", "D4", "E4","F4","G4","G4"]
+v5 = ["A4","A4", "A4", "A4","G4","G4"]
+v6 = ["F4", "F4", "F4","F4","E4","E4"]
+v7 = ["D4","D4","D4","E4","C4","C4"]
 
-song = [v1,v2,v3,v2]
+song = [v1,v2,v3,v3,v1,v2,v4,v5,v6,v7]
+
 
 for verse in song:
     for note in verse:
@@ -22,4 +22,3 @@ for verse in song:
         t.stop()
         sleep(0.1)
     sleep(0.2)
-    """
