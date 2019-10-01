c6a38297f0496a1dc32ea2bacbd5fb84d0231690
Pablo <p.gonzalez7027@gmail.com>
2019-10-01 14:46:26 -0500

    agregada pohLed al folder module

--- modules/pohLed.py
+++ modules/pohLed.py
@@ -1,84 +1,61 @@
-import gpiozero as GPIO
-import time
-from signal import pause
+from gpiozero import LED,PWMLED
+from datetime import datetime, timedelta
+import time 
 
-led = LED(26)
-button = Button(3)
+class Luz():
+    def __init__(self, pin):
+        self.pin = pin
 
-button.when_pressed = led.on
-button.when_released = led.off
+    def Encender(self):
+        led = LED(self. pin)
+        led.on()
+        print("Todo Bien")
+        
+    def Apagar(self):
+        led = LED(self.pin)
+        if LED.is_active == True:
+            led.off()
+        else:
+            pass
+    
+    def Parpadeo(self):
+        led = LED(self.pin)
+        while led.on == True:
+            timeout = 20   # [seconds]
+            timeout_start = time.time()
+            test = 0
+            print(datetime.now(),"Contando")
+            while time.time() < timeout_start + timeout:
+                if test == 20:
+                    print("Works")
+                     #print(datetime.now())
+                     #break
+                test = test + 1
+                #print(datetime.now())
+            led.n(10)
+            print(datetime.now(),"Dispositivo apagandose") 
 
-pause()
+    def Brillo(self):
+        led = LED(self.pin)
+        while led.on == True:
+            tiempo = 10   # [seconds]
+            timeout_start = time.time()
+            test = 0
+            print(datetime.now(),"Contando")
+            while time.time() < timeout_start + tiempo:
+                if test == 20:
+                    print("Works 2")
+                    #print(datetime.now())
+                    #break
+                test = test + 1
+                #print(datetime.now())
+            print(datetime.now(),"Brillo del LED?")
+            brillo1 = int(input())
+            brillopwm = brillo1/100.0
+            print(str(brillopwm))
+            PWMLED.intial_value(brillopwm)
+            print("Brillo regulado a:%s" %brillo1)
 
-try:
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(26,GPIO.OUT, initial = GPIO.LOW)
-print(“Printing SOS”)
 
-#S
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
 
-time.sleep(0.5)
 
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-
-time.sleep(0.5)
-
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-#End of S
-
-time.sleep(1.5)
-
-#O
-GPIO.output(26,GPIO.HIGH)
-time.sleep(1.5)
-GPIO.output(26,GPIO.LOW)
-
-time.sleep(0.5)
-
-GPIO.output(26,GPIO.HIGH)
-time.sleep(1.5)
-GPIO.output(26,GPIO.LOW)
-
-time.sleep(0.5)
-
-GPIO.output(26,GPIO.HIGH)
-time.sleep(1.5)
-GPIO.output(26,GPIO.LOW)
-#End of O
-
-time.sleep(1.5)
-
-#S
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-
-time.sleep(0.5)
-
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-
-time.sleep(0.5)
-
-GPIO.output(26,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(26,GPIO.LOW)
-#End of S
-
-time.sleep(1.5)
-
-print(“Finished”)
-
-except Exception as ex:
-print(“Error Occured”, ex)
-finally:
-GPIO.cleanup()
