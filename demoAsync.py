94430c20bff96c17dc7f14c574d4e937a65e116c
open507pty <panamaopenhub@gmail.com>
2019-10-04 20:37:38 +0100

    funcionando demo async de sensores

--- demoAsync.py
+++ demoAsync.py
@@ -1,36 +1,32 @@
 # countasync.py
 import asyncio
 import time
-import pohLcd as pantalla
-import pohLed as luz
+import pohLcd 
+import pohLed 
+import pohBuzzer
 
-async def count():
-    Led1 = luz.Luz(26)
-    Led1.Encender()
-    await asyncio.sleep(2)
-    Led1.Encender()
-    await asyncio.sleep(3)
-    Led1.Apagar()
-    await asyncio.sleep(2)
-    #Led1.Parpadeo()
-    await asyncio.sleep(1)
-    print("TAREA DEL LED COMPLETADA")
-    
-async def count2():
-    display = pantalla.Pantalla()
-    display.Bienvenida()
-    await asyncio.sleep(2)
-    display.FechaHora()
-    display.Limpiar()
-    await asyncio.sleep(1)
-    print("TAREA DE LA PANTALLA COMPLETADA")
+led1 = pohLed.Luz(26)
+bocina1 = pohBuzzer.Zumi2c()
+pantalla1 = pohLcd.Pantalla()
 
 
 async def main():
-    await asyncio.gather(count(), count2())
+    tareaLed = asyncio.create_task(led1.Parpadeo_Async(0.75))
+    tareaBocina = asyncio.create_task(bocina1.Test_Async())
+    tareaPantalla = asyncio.create_task(pantalla1.MostrarFechaHora_async(True))
+    await tareaLed
+    await tareaBocina
+    await tareaPantalla
 
 if __name__ == "__main__":
-    s = time.perf_counter()
-    asyncio.run(main())
-    elapsed = time.perf_counter() - s
-    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+    try:
+        s = time.perf_counter()
+        asyncio.run(main())
+        elapsed = time.perf_counter() - s
+        print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Closing Loop")
+    
+
